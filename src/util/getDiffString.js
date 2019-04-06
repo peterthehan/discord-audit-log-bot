05b95c6a1a26ee2ad631287c196e001dfe14c76e
Peter Han <peter.han.9493@gmail.com>
2019-04-06 18:03:13 -0400

    Stick with consistent output, use italics and bold for better visibility on mobile

--- src/util/getDiffString.js
+++ src/util/getDiffString.js
@@ -2,26 +2,22 @@ const jsdiff = require('diff');
 const applyStyle = require('./applyStyle');
 
 module.exports = (oldMessage, newMessage) => {
-  if (
-    ['~~', '*'].some(style =>
-      `${oldMessage.content}${newMessage.content}`.includes(style)
-    )
-  ) {
-    return `Old: ${oldMessage.content}\nNew: [${newMessage.content}](${
-      newMessage.url
-    })`;
-  }
-
   const diff = jsdiff
     .diffWordsWithSpace(oldMessage.content, newMessage.content)
-    .reduce(
-      (diffString, part) =>
-        (diffString += applyStyle(
-          part.value,
-          part.added ? '**' : part.removed ? '~~' : ''
-        )),
-      ''
-    );
+    .reduce((diffString, part) => {
+      const startsWithSpace = part.value.startsWith(' ');
+      const endsWithSpace = part.value.endsWith(' ');
+      part.value = part.value.trim().replace(/[*|~~]/g, '');
+
+      if (startsWithSpace) diffString += ' ';
+      diffString += applyStyle(
+        part.value,
+        part.added ? '***' : part.removed ? '~~' : ''
+      );
+      if (endsWithSpace) diffString += ' ';
+
+      return diffString;
+    }, '');
 
   return `${diff} [[link]](${newMessage.url})`;
 };
