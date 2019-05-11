2c8c0d25e6286b1131cbc19a7cf7ad5fb29f4047
Peter Han <peter.han.9493@gmail.com>
2019-05-11 14:57:40 -0400

    New color in config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   token: ('TOKEN' in process.env ? process.env : require('../config')).TOKEN,
+  addColor: 3066993,
   editColor: 15105570,
   deleteColor: 15277667,
   deleteTimeThreshold: 1,
