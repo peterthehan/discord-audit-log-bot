bb691a9b33f1e156a19b7b609db9b3b0a1ef7ee1
Peter Han <peter.han.9493@gmail.com>
2019-06-14 19:25:24 -0400

    Add user device presense

--- src/util/getFooter.js
+++ src/util/getFooter.js
@@ -1,3 +1,17 @@
-module.exports = (user, text) => ({
-  footer: { icon_url: user.displayAvatarURL(), text }
-});
+module.exports = (user, text) => {
+  const devices = user.presence.clientStatus
+    ? Object.keys(user.presence.clientStatus)
+    : [];
+
+  let footerText;
+  if (devices.length) {
+    const deviceMap = { web: '🌐', mobile: '📱', desktop: '💻' };
+    footerText = `${text} | ${devices
+      .map(device => deviceMap[device])
+      .join('')}`;
+  } else {
+    footerText = text;
+  }
+
+  return { footer: { icon_url: user.displayAvatarURL(), text: footerText } };
+};
