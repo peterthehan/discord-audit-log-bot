9ab7fff9d5533a1f5291ba1b6420dcc1460e6d5c
Peter Han <peter.han.9493@gmail.com>
2019-06-18 20:37:34 -0400

    Refactor guild check

--- src/util/getAuditLogChannel.js
+++ src/util/getAuditLogChannel.js
@@ -1,9 +1,12 @@
 const { guildChannelMap } = require('../config');
+const isInvalidGuild = require('./isInvalidGuild');
 
 const auditLogChannelCache = {};
 
-module.exports = async ({ client, id }) => {
-  if (!(id in guildChannelMap)) return;
+module.exports = async guild => {
+  if (isInvalidGuild(guild)) return;
+
+  const { client, id } = guild;
   if (!(id in auditLogChannelCache)) {
     auditLogChannelCache[id] = await client.channels.fetch(guildChannelMap[id]);
   }
