a5312a9eb1690134f9cfae431ad536c448583d22
Peter Han <peter.han.9493@gmail.com>
2019-06-20 22:49:53 -0400

    Add elapsed time to voice

--- src/handlers/voiceStateUpdate.js
+++ src/handlers/voiceStateUpdate.js
@@ -1,7 +1,11 @@
 const { positiveColor, neutralColor, negativeColor } = require('../config');
 const getDescription = require('../util/getDescription');
+const getElapsedTime = require('../util/getElapsedTime');
 const getFooter = require('../util/getFooter');
+const humanizeTime = require('../util/humanizeTime');
 const sendLog = require('../util/sendLog');
+
+const voiceCache = {};
 
 module.exports = (oldState, newState) => {
   const key =
@@ -10,21 +14,39 @@ module.exports = (oldState, newState) => {
       : Boolean(newState.channel).toString();
   if (key === 'null' && oldState.channel.id === newState.channel.id) return;
 
+  let elapsedTimeText = '';
+  if (key === 'true') {
+    voiceCache[newState.member.user.id] = Date.now();
+  } else {
+    if (newState.member.user.id in voiceCache) {
+      const humanizedElapsedTime = humanizeTime(
+        getElapsedTime(voiceCache[newState.member.user.id])
+      );
+      delete voiceCache[newState.member.user.id];
+
+      elapsedTimeText = ` after ${humanizedElapsedTime}`;
+    }
+
+    if (key === 'null') {
+      voiceCache[newState.member.user.id] = Date.now();
+    }
+  }
+
   const state = {
-    null: {
-      color: neutralColor,
-      description: `${oldState.channel} ➡️ ${newState.channel}`,
-      text: 'Changed'
-    },
     true: {
       color: positiveColor,
       description: `${newState.channel}`,
-      text: 'Joined'
+      text: 'Joined voice'
     },
     false: {
       color: negativeColor,
       description: `${oldState.channel}`,
-      text: 'Left'
+      text: `Left voice${elapsedTimeText}`
+    },
+    null: {
+      color: neutralColor,
+      description: `${oldState.channel} ➡️ ${newState.channel}`,
+      text: `Changed voice${elapsedTimeText}`
     }
   };
   if (!(key in state)) return;
@@ -33,6 +55,6 @@ module.exports = (oldState, newState) => {
 
   sendLog(newState.member.guild, color, {
     ...getDescription(newState.member.user, description),
-    ...getFooter(newState.member.user, `${text} voice`)
+    ...getFooter(newState.member.user, text)
   });
 };
