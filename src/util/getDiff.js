0acc0cee261d039aa62e5da72b2bf7a045c34015
Peter Han <peter.han.9493@gmail.com>
2019-11-03 14:46:23 -0500

    Choose diff result that yields smallest string

--- src/util/getDiff.js
+++ src/util/getDiff.js
@@ -2,19 +2,30 @@ const jsdiff = require('diff');
 
 const markdownRegExp = /\*|~~/g;
 
-const _applyStyle = (string, style) => `${style}${string}${style}`;
+const applyStyle = (string, style) => `${style}${string}${style}`;
 
-module.exports = (oldString, newString) =>
-  jsdiff
-    .diffWordsWithSpace(
-      oldString.replace(markdownRegExp, ''),
-      newString.replace(markdownRegExp, '')
-    )
-    .reduce((diffString, part) => {
-      diffString += _applyStyle(
-        part.value,
-        part.added ? '***' : part.removed ? '~~' : ''
-      );
+const getSmallestString = strings =>
+  strings.reduce((smallestString, currentString) =>
+    currentString.length < smallestString.length
+      ? currentString
+      : smallestString
+  );
 
-      return diffString;
-    }, '');
+module.exports = (oldString, newString) => {
+  const diffs = [jsdiff.diffWordsWithSpace, jsdiff.diffLines].map(
+    diffFunction =>
+      diffFunction(
+        oldString.replace(markdownRegExp, ''),
+        newString.replace(markdownRegExp, '')
+      ).reduce((diffString, part) => {
+        diffString += applyStyle(
+          part.value,
+          part.added ? '***' : part.removed ? '~~' : ''
+        );
+
+        return diffString;
+      }, '')
+  );
+
+  return getSmallestString(diffs);
+};
