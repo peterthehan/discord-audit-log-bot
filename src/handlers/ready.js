9def78ff250c8c965075f3243f4b8bd7de800d89
Peter Han <peter.han.9493@gmail.com>
2019-12-03 18:58:16 -0500

    Add package name to ready

--- src/handlers/ready.js
+++ src/handlers/ready.js
@@ -1,9 +1,10 @@
+const { name } = require('../../package');
 const { guildChannelMap } = require('../config');
 const AuditLogEmbedBuilder = require('../classes/AuditLogEmbedBuilder');
 const send = require('../util/send');
 
 module.exports = client => {
-  console.log(`${client.user.tag}: Ready`);
+  console.log(`${name}|${client.user.tag}: Ready`);
   for (const guildId of Object.keys(guildChannelMap)) {
     const guild = client.guilds.resolve(guildId);
     if (!guild) continue;
