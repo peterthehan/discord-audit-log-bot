473d186eb7934bcaa080d064eda6c1579b83c431
Peter Han <peter.han.9493@gmail.com>
2020-02-16 16:27:30 -0500

    Refactor member join/leave

--- src/handlers/guildMemberAdd.js
+++ src/handlers/guildMemberAdd.js
@@ -1,3 +1,6 @@
+const {
+  colors: { positiveColor }
+} = require('../config');
 const AuditLogEmbedBuilder = require('../classes/AuditLogEmbedBuilder');
 const Time = require('../classes/Time');
 const isLoggedGuild = require('../util/isLoggedGuild');
@@ -7,11 +10,11 @@ module.exports = guildMember => {
   if (!isLoggedGuild(guildMember.guild)) return;
 
   const time = new Time(guildMember.user.createdAt);
-  const embed = new AuditLogEmbedBuilder()
-    .setColor('positiveColor')
+  const builder = new AuditLogEmbedBuilder()
+    .setColor(positiveColor)
     .setUser(guildMember.user)
     .setBody(`Account created: ${time.toCleanISOString()}`)
     .setFooter('Joined the server');
 
-  send(guildMember.guild, embed);
+  send(guildMember.guild, builder);
 };
--- src/handlers/guildMemberRemove.js
+++ src/handlers/guildMemberRemove.js
@@ -1,3 +1,6 @@
+const {
+  colors: { negativeColor }
+} = require('../config');
 const AuditLogEmbedBuilder = require('../classes/AuditLogEmbedBuilder');
 const Time = require('../classes/Time');
 const isLoggedGuild = require('../util/isLoggedGuild');
@@ -7,11 +10,11 @@ module.exports = guildMember => {
   if (!isLoggedGuild(guildMember.guild)) return;
 
   const time = new Time(guildMember.joinedAt);
-  const embed = new AuditLogEmbedBuilder()
-    .setColor('negativeColor')
+  const builder = new AuditLogEmbedBuilder()
+    .setColor(negativeColor)
     .setUser(guildMember.user)
     .setBody(`Joined server: ${time.toCleanISOString()}`)
     .setFooter(`Left the server after ${time.getHumanizedElapsedTime()}`);
 
-  send(guildMember.guild, embed);
+  send(guildMember.guild, builder);
 };
