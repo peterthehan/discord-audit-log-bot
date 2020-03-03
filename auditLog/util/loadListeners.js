5fba94aeb4cc6e1d6e3740c0923e33e59f64443c
Peter Han <peter.han.9493@gmail.com>
2020-03-02 21:27:13 -0500

    Remove unused loader

--- auditLog/util/loadListeners.js
+++
@@ -1,9 +0,0 @@
-const { events } = require('../config');
-
-const handler = event => require(`../handlers/${event}`);
-
-module.exports = client => {
-  client.once('ready', () => handler('ready')(client));
-  process.on('unhandledRejection', console.warn);
-  events.forEach(event => client.on(event, handler(event)));
-};
