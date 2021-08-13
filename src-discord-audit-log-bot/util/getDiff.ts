5080d93d4b7950a85ef10c19dc6605aff5e776f0
Peter Han <peter.han.9493@gmail.com>
2021-08-12 18:47:00 -0700

    Update diff logic

--- src-discord-audit-log-bot/util/getDiff.ts
+++ src-discord-audit-log-bot/util/getDiff.ts
@@ -1,11 +1,38 @@
-import { diffLines, diffWordsWithSpace } from "diff";
+import { Change, diffLines, diffWordsWithSpace } from "diff";
 import { Util } from "discord.js";
+
+function mapPartTypeToStyle(part: Change): string {
+  return part.added ? " _**" : part.removed ? " ~~" : "";
+}
+
+function mergeChanges(merged: Change[], part: Change): Change[] {
+  if (
+    !merged.length ||
+    (mapPartTypeToStyle(merged[merged.length - 1]) !==
+      mapPartTypeToStyle(part) &&
+      !/^\s+$/.test(part.value))
+  ) {
+    merged.push(part);
+  } else {
+    merged[merged.length - 1].value += part.value;
+  }
+
+  return merged;
+}
 
 function reverse(string: string): string {
   return [...string].reverse().join("");
 }
 
 function applyStyle(string: string, style: string): string {
+  if (string.startsWith(" ")) {
+    string = string.slice(1);
+  }
+
+  if (string.endsWith(" ")) {
+    string = string.slice(0, string.length - 1);
+  }
+
   return `${style}${string}${reverse(style)}`;
 }
 
@@ -18,16 +45,16 @@ function getSmallestString(strings: string[]): string {
 }
 
 function getDiff(oldString: string, newString: string): string {
+  // escape markdown on messages to avoid conflict with diff markdown
   oldString = Util.escapeMarkdown(oldString);
   newString = Util.escapeMarkdown(newString);
 
   const diffs = [diffLines, diffWordsWithSpace].map((diffFunction) =>
-    diffFunction(oldString, newString).reduce((diffString, part) => {
-      return (diffString += applyStyle(
-        part.value,
-        part.added ? "**_" : part.removed ? "~~" : ""
-      ));
-    }, "")
+    diffFunction(oldString, newString)
+      .reduce(mergeChanges, [] as Change[])
+      .reduce((diffString, part) => {
+        return (diffString += applyStyle(part.value, mapPartTypeToStyle(part)));
+      }, "")
   );
 
   return getSmallestString(diffs);
