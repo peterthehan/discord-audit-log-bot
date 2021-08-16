21b91fd583e9632ec4c6d4b8ded82f15ba77626d
Peter Han <peter.han.9493@gmail.com>
2021-08-15 18:00:33 -0700

    Clean up intents

--- src-discord-audit-log-bot/intents.ts
+++ src-discord-audit-log-bot/intents.ts
@@ -2,7 +2,6 @@ import { Intents } from "discord.js";
 
 module.exports = [
   Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGES,
-  Intents.FLAGS.GUILD_PRESENCES,
   Intents.FLAGS.GUILD_MEMBERS,
+  Intents.FLAGS.GUILD_MESSAGES,
 ];
